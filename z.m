cd129f7b64b24f5011df28ea0043d6c4f3936c0b
Zafar <zafarrafii@gmail.com>
2018-05-02 09:33:55 -0700

    Little update

--- z.m
+++ z.m
@@ -148,7 +148,7 @@
             %       audiowrite('center_signal.wav',center_signal,sample_rate);
             %       audiowrite('sides_signal.wav',sides_signal,sample_rate);
             %       
-            %       % Original, center, and sides signals
+            %       % Original, center, and sides signals displayed in s
             %       figure
             %       subplot(3,1,1), plot(audio_signal), axis tight, title('Original Signal')
             %       xticks(sample_rate:sample_rate:length(audio_signal))
@@ -839,7 +839,7 @@
             %       audio_signal2 = audio_signal2(1:length(audio_signal));
             %       error_signal = audio_signal-audio_signal2;
             %       
-            %       % Original, resynthesized, and error signals
+            %       % Original, resynthesized, and error signals displayed in s
             %       figure
             %       subplot(3,1,1), plot(audio_signal), axis tight, title('Original Signal')
             %       xticks(sample_rate:sample_rate:length(audio_signal))
