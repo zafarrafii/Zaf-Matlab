75926eacacc14dba303560a5d7c89c04dd8f9003
Zafar <zafarrafii@gmail.com>
2018-05-16 13:17:30 -0700

    Finished stft in julia.

--- z.jl
+++ z.jl
@@ -1,7 +1,7 @@
 """
-This module implements several functions for audio signal processing.
+`z` This module implements several functions for audio signal processing.
 
-Functions:
+`z` Functions:
 - `stft` - Short-time Fourier transform (STFT)
 - `istft` - inverse STFT
 - `cqtkernel` - Constant-Q transform (CQT) kernel
@@ -19,12 +19,12 @@ Author:
 - http://zafarrafii.com
 - https://github.com/zafarrafii
 - https://www.linkedin.com/in/zafarrafii/
-- 05/15/18
+- 05/16/18
 """
 module z
 
 # Public
-export stft, istft, test
+export stft, istft, test, hamming
 
 """
     audio_stft = z.stft(audio_signal, window_function, step_length)
@@ -32,34 +32,76 @@ export stft, istft, test
 Compute the short-time Fourier transform (STFT)
 
 # Arguments
-- `audio_signal`: audio signal [number_samples, 0]
-- `window_function`: window function [window_length, 0]
-- `step_length`: step length in samples
-- `audio_stft`: audio STFT [window_length, number_frames]
+- `audio_signal`: the audio signal [number_samples, 1]
+- `window_function::Integer`: the window function [window_length, 1]
+- `step_length`: the step length in samples
+- `audio_stft`: the audio STFT [window_length, number_frames]
 
-# Example Compute and display the spectrogram of an audio file
-```
+# Example
+
+```jldoctest
+julia> a = [1 2; 3 4]
+2×2 Array{Int64,2}:
+ 1  2
+ 3  4
 ```
 """
 function stft(audio_signal,window_funciton,step_length)
 
     # Number of samples and window length
-   number_samples = length(audio_signal)
-   window_length = length(window_function)
+    number_samples = length(audio_signal)
+    window_length = length(window_function)
 
-   # Number of time number_frames
-   number_times = ceil((window_length-step_length+number_samples)/step_length)
+    # Number of time number_frames
+    number_times = ceil((window_length-step_length+number_samples)/step_length)
+
+    # Zero-padding at the start and end to center the windows
+    audio_signal = [zeros(window_length-step_length,1);audio_signal;
+        zeros(number_times*step_length-number_samples,1)]
+
+    # Initialize the STFT
+    audio_stft = zeros(window_length,number_times)
+
+    # Loop over the time frames
+    for time_index = 1:number_times
+
+        # Window the signal
+        sample_index = step_length*(time_index-1)
+        audio_stft[:,time_index] = audio_signal[1+sample_index:window_length+sample_index].*window_function
+
+        end
+
+    # Fourier transform of the frames
+    audio_stft = fft(audio_stft)
 
 end
 
-"istft Inverse short-time Fourier transform (STFT)"
+"Compute the inverse short-time Fourier transform (STFT)"
 function istft()
    println("this is stft")
 end
 
+"Compute the Hamming window"
+function hamming(window_length,window_sampling="symmetric")
+
+    if window_sampling == "symmetric"
+        window_function = 0.54 - 0.46*cos(2*pi*(0:window_length-1)/(window_length-1))
+    elseif window_sampling == "periodic"
+        window_function = 0.54 - 0.46*cos(2*pi*(0:window_length-1)/window_length)
+    else
+        error("Window sampling must be either 'symmetric or 'periodic'.")
+    end
+
+end
+
 function test()
-    a = a+2
-    a*b
+
+    # Pkg.add("WAV")
+    using WAV
+    audio_signal, sample_rate = wavread("audio_file.wav")
+
+    using z
+    audio_signal = stft()
 
 end
 
