5d631e20aa604250ce124182e88a0f585ea9cf6b
Zafar <zafarrafii@gmail.com>
2018-05-18 11:59:14 -0700

    Updated julia z stft.

--- z.jl
+++ z.jl
@@ -19,7 +19,7 @@ Author:
 - http://zafarrafii.com
 - https://github.com/zafarrafii
 - https://www.linkedin.com/in/zafarrafii/
-- 05/17/18
+- 05/18/18
 """
 module z
 
@@ -31,19 +31,37 @@ export stft, istft, test
 
 Compute the short-time Fourier transform (STFT)
 
-# Arguments
+# Arguments:
 - `audio_signal::Float`: the audio signal [number_samples, 1]
 - `window_function::Integer`: the window function [window_length, 1]
 - `step_length::Integer`: the step length in samples
 - `audio_stft::Float`: the audio STFT [window_length, number_frames]
 
-# Example
+# Example: Compute the spectrogram of an audio file
+```
+# Use Julia package for working with WAV files
+Pkg.add("WAV")
+using WAV
 
-```jldoctest
-julia> a = [1 2; 3 4]
-2×2 Array{Int64,2}:
- 1  2
- 3  4
+# Audio signal averaged over its channels and sample rate in Hz
+audio_signal, sample_rate = wavread("audio_file.wav");
+audio_signal = mean(audio_signal,2);
+
+# Window duration in seconds (audio is stationary around 40 milliseconds)
+window_duration = 0.04;
+
+# Window length in samples (power of 2 for fast FFT and constant overlap-add (COLA))
+window_length = nextpow2(convert(Int64,window_duration*sample_rate));
+
+# Window function (periodic Hamming window for COLA)
+window_function = hamming(window_length,"periodic");
+
+# Step length in samples (half the window length for COLA)
+step_length = convert(Int64,window_length/2);
+
+# Magnitude spectrogram (without the DC component and the mirrored frequencies)
+audio_stft = z.stft(audio_signal,window_function,step_length);
+audio_spectrogram = abs.(audio_stft[2:convert(Int64,window_length/2)+1,:]);
 ```
 """
 function stft(audio_signal,window_function,step_length)
@@ -78,16 +96,16 @@ end
 
 "Compute the inverse short-time Fourier transform (STFT)"
 function istft()
-   println("this is stft")
+   println("this is istft")
 end
 
 "Compute the Hamming window"
 function hamming(window_length,window_sampling="symmetric")
 
     if window_sampling == "symmetric"
-        window_function = 0.54 - 0.46*cos(2*pi*(0:window_length-1)/(window_length-1))
+        window_function = 0.54 - 0.46*cos.(2*pi*(0:window_length-1)/(window_length-1))
     elseif window_sampling == "periodic"
-        window_function = 0.54 - 0.46*cos(2*pi*(0:window_length-1)/window_length)
+        window_function = 0.54 - 0.46*cos.(2*pi*(0:window_length-1)/window_length)
     else
         error("Window sampling must be either 'symmetric or 'periodic'.")
     end
@@ -100,13 +118,25 @@ using WAV
 
 function test()
 
-    audio_signal, sample_rate = wavread("audio_file.wav")
-    audio_signal = mean(audio_signal,2)
+    # Audio signal averaged over its channels and sample rate in Hz
+    audio_signal, sample_rate = wavread("audio_file.wav");
+    audio_signal = mean(audio_signal,2);
 
-    window_length = 2048
-    step_length = convert(Int64,window_length/2)
-    window_function = hamming(window_length,"periodic")
-    audio_stft = stft(audio_signal,window_function,step_length)
+    # Window duration in seconds (audio is stationary around 40 milliseconds)
+    window_duration = 0.04;
+
+    # Window length in samples (power of 2 for fast FFT and constant overlap-add (COLA))
+    window_length = nextpow2(convert(Int64,window_duration*sample_rate));
+
+    # Window function (periodic Hamming window for COLA)
+    window_function = hamming(window_length,"periodic");
+
+    # Step length in samples (half the window length for COLA)
+    step_length = convert(Int64,window_length/2);
+
+    # Magnitude spectrogram (without the DC component and the mirrored frequencies)
+    audio_stft = z.stft(audio_signal,window_function,step_length);
+    audio_spectrogram = abs.(audio_stft[2:convert(Int64,window_length/2)+1,:]);
 
 end
 
