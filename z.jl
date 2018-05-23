d734b189d3da51773a762486284a6d633588e6c6
Zafar <zafarrafii@gmail.com>
2018-05-22 18:00:42 -0700

    Finished julia z istft.

--- z.jl
+++ z.jl
@@ -19,7 +19,7 @@ Author:
 - http://zafarrafii.com
 - https://github.com/zafarrafii
 - https://www.linkedin.com/in/zafarrafii/
-- 05/21/18
+- 05/22/18
 """
 module z
 
@@ -116,7 +116,7 @@ Compute the inverse short-time Fourier transform (STFT)
 # Example: Estimate the center and sides signals of a stereo audio file
 ```
 # Stereo audio signal and sample rate in Hz
-#Pkg.add("WAV")
+Pkg.add("WAV")
 using WAV
 audio_signal, sample_rate = wavread("audio_file.wav");
 
@@ -129,24 +129,24 @@ step_length = convert(Int64, window_length/2);
 # STFT of the left and right channels
 include("z.jl")
 using z
-audio_stft1 = z.stft(audio_signal[:,1], window_function, step_length);
-audio_stft2 = z.stft(audio_signal[:,2], window_function, step_length);
+audio_stft1 = stft(audio_signal[:,1], window_function, step_length);
+audio_stft2 = stft(audio_signal[:,2], window_function, step_length);
 
 # Magnitude spectrogram (with DC component) of the left and right channels
 audio_spectrogram1 = abs.(audio_stft1[1:Int(window_length/2)+1, :]);
 audio_spectrogram2 = abs.(audio_stft2[1:Int(window_length/2)+1, :]);
 
 # Time-frequency masks of the left and right channels for the center signal
-center_mask1 = min(audio_spectrogram1, audio_spectrogram2)./audio_spectrogram1;
-center_mask2 = min(audio_spectrogram1, audio_spectrogram2)./audio_spectrogram2;
+center_mask1 = min.(audio_spectrogram1, audio_spectrogram2)./audio_spectrogram1;
+center_mask2 = min.(audio_spectrogram1, audio_spectrogram2)./audio_spectrogram2;
 
 # STFT of the left and right channels for the center signal (with extension to mirrored frequencies)
 center_stft1 = cat(1, center_mask1, center_mask1[Int(window_length/2):-1:2,:]).*audio_stft1;
 center_stft2 = cat(1, center_mask2, center_mask2[Int(window_length/2):-1:2,:]).*audio_stft2;
 
 # Synthesized signals of the left and right channels for the center signal
-center_signal1 = z.istft(center_stft1, window_function, step_length);
-center_signal2 = z.istft(center_stft2, window_function, step_length);
+center_signal1 = istft(center_stft1, window_function, step_length);
+center_signal2 = istft(center_stft2, window_function, step_length);
 
 # Final stereo center and sides signals
 center_signal = cat(2, center_signal1, center_signal2);
@@ -158,13 +158,14 @@ wavwrite(center_signal, "center_signal.wav", Fs=sample_rate);
 wavwrite(sides_signal, "sides_signal.wav", Fs=sample_rate);
 
 # Spectrogram displayed in dB, s, and kHz
-#Pkg.add("Plots")
+Pkg.add("Plots")
 using Plots
 plotly()
-p1 = plot(audio_signal, xlabel="Time (s)", title="Original Signal");
-p2 = plot(center_signal, xlabel="Time (s)", title="Center Signal");
-p3 = plot(sides_signal, xlabel="Time (s)", title="Sides Signal");
-plot(p1, p2, p3,layout=(3,1))
+time_signal = (1:size(audio_signal, 1))/sample_rate;
+audio_plot = plot(time_signal, audio_signal, xlabel="Time (s)", title="Original Signal");
+center_plot = plot(time_signal, center_signal, xlabel="Time (s)", title="Center Signal");
+sides_plot = plot(time_signal, sides_signal, xlabel="Time (s)", title="Sides Signal");
+plot(audio_plot, center_plot, sides_plot, layout=(3,1), legend=false)
 ```
 """
 function istft(audio_stft, window_function, step_length)
@@ -179,7 +180,7 @@ function istft(audio_stft, window_function, step_length)
     audio_signal = zeros(number_samples, 1);
 
     # Inverse Fourier transform of the frames and real part to ensure real values
-    audio_stft = real(ifft(audio_stft));
+    audio_stft = real(ifft(audio_stft, 1));
 
     # Loop over the time frames
     for time_index = 1:number_times
