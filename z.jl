f3f2b2de1b3500c270d8a8a6d9486c4f6d779121
Zafar <zafarrafii@gmail.com>
2018-05-24 11:59:56 -0700

    Updated julia z cqtkernel.

--- z.jl
+++ z.jl
@@ -19,12 +19,12 @@ Author:
 - http://zafarrafii.com
 - https://github.com/zafarrafii
 - https://www.linkedin.com/in/zafarrafii/
-- 05/22/18
+- 05/24/18
 """
 module z
 
 # Public
-#export stft, istft
+#export stft, istft, cqtkernel
 
 """
     audio_stft = z.stft(audio_signal, window_function, step_length);
@@ -35,7 +35,7 @@ Compute the short-time Fourier transform (STFT)
 - `audio_signal::Float`: the audio signal [number_samples, 1]
 - `window_function::Integer`: the window function [window_length, 1]
 - `step_length::Integer`: the step length in samples
-- `audio_stft::Float`: the audio STFT [window_length, number_frames]
+- `audio_stft::Complex`: the audio STFT [window_length, number_frames]
 
 # Example: Compute the spectrogram of an audio file
 ```
@@ -49,17 +49,17 @@ audio_signal = mean(audio_signal, 2);
 window_duration = 0.04;
 
 # Window length in samples (power of 2 for fast FFT and constant overlap-add (COLA))
-window_length = nextpow2(convert(Int64, window_duration*sample_rate));
+window_length = nextpow2(ceil(Int64, window_duration*sample_rate));
 
 # Window function (periodic Hamming window for COLA)
-window_function = 0.54 - 0.46*cos.(2*pi*(0:window_length-1)/window_length);
+include("z.jl")
+using z
+window_function = z.hamming(window_length, "periodic");
 
 # Step length in samples (half the window length for COLA)
 step_length = convert(Int64, window_length/2);
 
 # Magnitude spectrogram (without the DC component and the mirrored frequencies)
-include("z.jl")
-using z
 audio_stft = z.stft(audio_signal, window_function, step_length);
 audio_spectrogram = abs.(audio_stft[2:convert(Int64, window_length/2)+1,:]);
 
@@ -72,7 +72,6 @@ y_labels = [string(round(i*sample_rate/window_length/1000, 2)) for i = 1:size(au
 heatmap(x_labels, y_labels, 20*log10.(audio_spectrogram))
 ```
 """
-
 function stft(audio_signal, window_function, step_length)
 
     # Number of samples and window length
@@ -109,7 +108,7 @@ end
 Compute the inverse short-time Fourier transform (STFT)
 
 # Arguments:
-- `audio_stft::Float`: the audio STFT [window_length, number_frames]
+- `audio_stft::Complex`: the audio STFT [window_length, number_frames]
 - `window_function::Integer`: the window function [window_length, 1]
 - `step_length::Integer`: the step length in samples
 - `audio_signal::Float`: the audio signal [number_samples, 1]
@@ -122,14 +121,14 @@ using WAV
 audio_signal, sample_rate = wavread("audio_file.wav");
 
 # Parameters for the STFT
+include("z.jl")
+using z
 window_duration = 0.04;
-window_length = nextpow2(convert(Int64, window_duration*sample_rate));
-window_function = 0.54 - 0.46*cos.(2*pi*(0:window_length-1)/window_length);
+window_length = nextpow2(ceil(Int64, window_duration*sample_rate));
+window_function = z.hamming(window_length,"periodic");
 step_length = convert(Int64, window_length/2);
 
 # STFT of the left and right channels
-include("z.jl")
-using z
 audio_stft1 = z.stft(audio_signal[:,1], window_function, step_length);
 audio_stft2 = z.stft(audio_signal[:,2], window_function, step_length);
 
@@ -188,7 +187,8 @@ function istft(audio_stft, window_function, step_length)
 
         # Constant overlap-add (if proper window and step)
         sample_index = step_length*(time_index-1);
-        audio_signal[1+sample_index:window_length+sample_index] = audio_signal[1+sample_index:window_length+sample_index] + audio_stft[:,time_index];
+        audio_signal[1+sample_index:window_length+sample_index] = audio_signal[1+sample_index:window_length+sample_index]
+        + audio_stft[:,time_index];
 
     end
 
@@ -200,58 +200,104 @@ function istft(audio_stft, window_function, step_length)
 
 end
 
+"""
+    cqt_kernel = z.cqtkernel(sample_rate, frequency_resolution, minimum_frequency, maximum_frequency);
+
+Compute the constant-Q transform (CQT) kernel
+
+# Arguments:
+- `sample_rate::Float` sample rate in Hz
+- `frequency_resolution::Float` frequency resolution in number of frequency channels per semitone
+- `minimum_frequency::Float`: minimum frequency in Hz
+- `maximum_frequency::Float`: maximum frequency in Hz
+- `cqt_kernel::Float`: CQT kernel [number_frequencies, fft_length]
+
+# Example: Compute and display the CQT kernel
+```
+# CQT kernel parameters
+sample_rate = 44100;
+frequency_resolution = 2;
+minimum_frequency = 55;
+maximum_frequency = sample_rate/2;
+
+# CQT kernel
+cqt_kernel = z.cqtkernel(sample_rate,frequency_resolution,minimum_frequency,maximum_frequency);
+
+# Magnitude CQT kernel displayed
+figure
+imagesc(abs(cqt_kernel))
+axis xy
+colormap(jet)
+title('Magnitude CQT kernel')
+xlabel('FFT length')
+ylabel('CQT frequency')
+set(gca,'FontSize',30)
+```
+"""
 function cqtkernel(sample_rate, frequency_resolution, minimum_frequency, maximum_frequency)
 
-# Number of frequency channels per octave
-octave_resolution = 12*frequency_resolution;
+    # Number of frequency channels per octave
+    octave_resolution = 12*frequency_resolution;
 
-# Constant ratio of frequency to resolution (= fk/(fk+1-fk))
-quality_factor = 1/(2^(1/octave_resolution)-1);
+    # Constant ratio of frequency to resolution (= fk/(fk+1-fk))
+    quality_factor = 1/(2^(1/octave_resolution)-1);
 
-# Number of frequency channels for the CQT
-number_frequencies = round(octave_resolution*log2(maximum_frequency/minimum_frequency));
+    # Number of frequency channels for the CQT
+    number_frequencies = round(Int64, octave_resolution*log2(maximum_frequency/minimum_frequency));
 
-# Window length for the FFT (= window length of the minimum frequency = longest window)
-fft_length = 2^nextpow2(quality_factor*sample_rate/minimum_frequency);
+    # Window length for the FFT (= window length of the minimum frequency = longest window)
+    fft_length = nextpow2(ceil(Int64, quality_factor*sample_rate/minimum_frequency));
 
-# Initialize the kernel
-cqt_kernel = zeros(number_frequencies,fft_length);
+    # Initialize the kernel
+    cqt_kernel = zeros(Complex128,number_frequencies, fft_length);
 
-# Loop over the frequency channels
-for frequency_index = 1:number_frequencies
+    # Loop over the frequency channels
+    #for frequency_index = 1:number_frequencies
 
-    # Frequency value (in Hz)
-    frequency_value = minimum_frequency*2^((frequency_index-1)/octave_resolution);
+        # Frequency value (in Hz)
+        #frequency_value = minimum_frequency*2^((frequency_index-1)/octave_resolution);
 
-    # Window length (nearest odd value because the complex exponential will have an odd length, in samples)
-    window_length = 2*round(quality_factor*sample_rate/frequency_value/2)+1;
+        # Window length (nearest odd value because the complex exponential will have an odd length, in samples)
+        #window_length = 2*round(Int64, quality_factor*sample_rate/frequency_value/2)+1;
 
-    # Temporal kernel (without zero-padding, odd and symmetric)
-    temporal_kernel = hamming(window_length,'symmetric')' ...
-        .*exp(2*pi*1j*quality_factor*(-(window_length-1)/2:(window_length-1)/2)/window_length)/window_length;
+        # Temporal kernel (without zero-padding, odd and symmetric)
+        #temporal_kernel = hamming(window_length, "symmetric").*exp.(2*pi*im*quality_factor*(-(window_length-1)/2:(window_length-1)/2)/window_length)/window_length;
 
-    # Pre zero-padding to center FFTs (fft does post zero-padding; temporal kernel still odd but almost symmetric)
-    temporal_kernel = cat(2,zeros(1,(fft_length-window_length+1)/2),temporal_kernel);
+        # Pre and post zero-padding to center FFTs
+        #temporal_kernel = cat(2, zeros(1, convert(Int64, (fft_length-window_length+1)/2)), temporal_kernel', zeros(1, convert(Int64, (fft_length-window_length-1)/2)));
 
-    # Spectral kernel (mostly real because temporal kernel almost symmetric)
-    spectral_kernel = fft(temporal_kernel,fft_length);
+        # Spectral kernel (mostly real because temporal kernel almost symmetric)
+        #spectral_kernel = fft(temporal_kernel, 1);
 
-    # Save the spectral kernels
-    cqt_kernel(frequency_index,:) = spectral_kernel;
+        # Save the spectral kernels
+        #cqt_kernel[frequency_index, :] = spectral_kernel;
+
+    #end
+
+    # Energy threshold for making the kernel sparse
+    #energy_threshold = 0.01;
+
+    # Make the CQT kernel sparser
+    #cqt_kernel(abs(cqt_kernel)<energy_threshold) = 0;
+
+    # Make the CQT kernel sparse
+    #cqt_kernel = sparse(cqt_kernel);
+
+    # From Parseval's theorem
+    #cqt_kernel = conj(cqt_kernel)/fft_length;
 
 end
 
-# Energy threshold for making the kernel sparse
-energy_threshold = 0.01;
+"Compute the Hamming window"
+function hamming(window_length,window_sampling="symmetric")
 
-# Make the CQT kernel sparser
-cqt_kernel(abs(cqt_kernel)<energy_threshold) = 0;
-
-# Make the CQT kernel sparse
-cqt_kernel = sparse(cqt_kernel);
-
-# From Parseval's theorem
-cqt_kernel = conj(cqt_kernel)/fft_length;
+    if window_sampling == "symmetric"
+        window_function = 0.54 - 0.46*cos.(2*pi*(0:window_length-1)/(window_length-1))
+    elseif window_sampling == "periodic"
+        window_function = 0.54 - 0.46*cos.(2*pi*(0:window_length-1)/window_length)
+    else
+        error("Window sampling must be either 'symmetric or 'periodic'.")
+    end
 
 end
 
